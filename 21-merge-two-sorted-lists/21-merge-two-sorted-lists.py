027b71a70430c4630d35d39bd2ff0f0a37485f48
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-07 18:12:07 +0200

    Time: 32 ms (53.67%), Space: 13.2 MB (95.72%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.py
@@ -14,23 +14,23 @@ class Solution(object):
             return list2
         if not list2:
             return list1
-        if list1.val <= list2.val:
-            res = list1
-            list1 = list1.next
-        else:
-            res = list2
-            list2 = list2.next
-        res.next = None
+        # if list1.val <= list2.val:
+        #     res = list1
+        #     list1 = list1.next
+        # else:
+        #     res = list2
+        #     list2 = list2.next
+        res = ListNode(0)
         head = res
         while list1 and list2:
             if list1.val <= list2.val:
                 res.next = list1
-                list1 = list1.next
                 res = res.next
+                list1 = list1.next
             else:
                 res.next = list2
                 res = res.next
                 list2 = list2.next
         if list1 or list2:
             res.next = list1 if list1 else list2
-        return head
+        return head.next
