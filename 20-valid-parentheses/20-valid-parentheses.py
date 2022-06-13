1a40a05f19f647887a2d35517811203ca8925bdb
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-14 01:02:28 +0200

    Time: 42 ms (11.80%), Space: 13.6 MB (59.21%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        x = True
+        if len(s)%2 != 0:
+            return False
+        for i in range(0, len(s)):
+            if s[i] in ['(', '[', '{']:
+                stack.append(s[i])
+            if s[i] in [')', ']', '}']:
+                try:
+                    x = stack.pop()
+                except:
+                    return False
+                if not (ord(s[i]) == ord(x)+1 or ord(s[i]) == ord(x)+2):
+                    print(x)
+                    return False
+        if len(stack) > 0:
+            return False
+            # if s[i] == '[' and s[i+1] != ']':
+            #     return False
+            # if s[i] == '{' and s[i+1] != '}':
+            #     return False
+            # print(i)
+        return True
