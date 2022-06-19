20e45fe49b0103fc56cfaf94e9ab797eb7e04db2
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-19 23:36:58 +0200

    Time: 69 ms (10.71%), Space: 14.1 MB (78.30%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        pos = 0
+        maxi = float('-inf')
+        for i in range(0,len(nums)):
+            if nums[i] == target:
+                return i
+            else:
+                if nums[i] < target and nums[i] > maxi:
+                    maxi = nums[i]
+                    pos = i + 1
+        return pos
