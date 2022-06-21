7bccfc71fecc8710b391cc47a84b8d2b5b69dda2
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-21 17:28:04 +0200

    Time: 27 ms (42.25%), Space: 13.5 MB (10.59%) - LeetHub

---
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        neg = False
+        if num2 < 0:
+            neg = True
+        for i in range(0, abs(num2)):
+            num1 = num1 - 1 if neg else num1 + 1
+        return num1
