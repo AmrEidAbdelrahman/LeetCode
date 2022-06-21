8f96c061281778c265e2d94949bed66b1250b72b
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-21 17:30:27 +0200

    Time: 24 ms (55.69%), Space: 13.2 MB (83.99%) - LeetHub

--- 2235-add-two-integers/2235-add-two-integers.py
+++ 2235-add-two-integers/2235-add-two-integers.py
@@ -5,13 +5,13 @@ class Solution(object):
         :type num2: int
         :rtype: int
         """
-        # neg = False
-        # if num2 < 0:
-        #     neg = True
-        # for i in range(0, abs(num2)):
-        #     num1 = num1 - 1 if neg else num1 + 1
-        # return num1
+        neg = False
+        if num2 < 0:
+            neg = True
+        for i in range(0, abs(num2)):
+            num1 = num1 - 1 if neg else num1 + 1
+        return num1
         
         # return num1 + num2
         
-        return num1.__add__(num2)
+        # return num1.__add__(num2)
