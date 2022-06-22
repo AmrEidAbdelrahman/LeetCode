9a3507aa86e5d7e40d47459b97acfec787905514
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-22 17:21:22 +0200

    Time: 106 ms (34.35%), Space: 13.9 MB (15.97%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -4,9 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums_len = len(nums)
-        res = [0 for i in range(0, nums_len*2)]
-        for i in range(0, nums_len):
-            res[i] = nums[i]
-            res[nums_len+i] = nums[i]
-        return res
+        # nums_len = len(nums)
+        # res = [0 for i in range(0, nums_len*2)]
+        # for i in range(0, nums_len):
+        #     res[i] = nums[i]
+        #     res[nums_len+i] = nums[i]
+        # return res
+        return nums * 2
