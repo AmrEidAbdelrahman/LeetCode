bcc39751f8b3f9fc1dc8a984472e1ed49f931617
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-23 00:14:51 +0200

    Time: 258 ms (33.68%), Space: 21 MB (92.84%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -4,5 +4,11 @@ class Solution(object):
         :type s: List[str]
         :rtype: None Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
-        
+        # return s.reverse()
+        l = len(s)-1
+        mid = len(s)//2
+        for i in range(0, mid):
+            cub = s[l-i]
+            s[l-i] = s[i]
+            s[i] = cub
+        return s
