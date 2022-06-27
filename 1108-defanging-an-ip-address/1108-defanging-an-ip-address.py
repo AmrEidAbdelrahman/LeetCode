55f7c9e5400710513f8613eefe666bd4a952a9c8
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-06-27 21:57:06 +0200

    Time: 38 ms (5.38%), Space: 13.4 MB (54.90%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.py
@@ -4,5 +4,11 @@ class Solution(object):
         :type address: str
         :rtype: str
         """
-        return address.replace('.', '[.]')
-        
+        # return address.replace('.', '[.]')
+        i = 0
+        while i < len(address):
+            if address[i] == '.':
+                address = address[0:i] + '[.]' + address[i+1:]
+                i += 3
+            i += 1
+        return address
