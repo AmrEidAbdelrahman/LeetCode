e7f8d14ae9dbd36e3ac99cc36a9fbc1a6009d08f
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-07-04 04:23:07 +0200

    Time: 69 ms (15.86%), Space: 13.6 MB (9.91%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -4,8 +4,15 @@ class Solution(object):
         :type operations: List[str]
         :rtype: int
         """
-        minus = operations.count('--X') + operations.count('X--')
-        plus = operations.count('++X') + operations.count('X++')
-        return plus - minus
+        # minus = operations.count('--X') + operations.count('X--')
+        # plus = operations.count('++X') + operations.count('X++')
+        # return plus - minus
+        res = 0
+        for op in operations:
+            if op == '--X' or op == 'X--':
+                res -= 1
+            elif op == '++X' or op == 'X++':
+                res += 1
+        return res
                 
         
