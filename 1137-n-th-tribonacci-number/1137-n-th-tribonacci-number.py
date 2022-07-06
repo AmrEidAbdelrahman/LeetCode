da959ca99bf494f2bb22421012a493ac81f23b93
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-07-06 13:59:06 +0200

    Time: 17 ms (84.65%), Space: 13.5 MB (10.87%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -10,5 +10,8 @@ class Solution(object):
             return n
         if n == 2:
             return 1
-        memo[n] = self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+        memo[n-3] = self.tribonacci(n-3) 
+        memo[n-2] = self.tribonacci(n-2) 
+        memo[n-1] = self.tribonacci(n-1)
+        memo[n] = memo[n-3] + memo[n-2] + memo[n-1]
         return memo[n]
