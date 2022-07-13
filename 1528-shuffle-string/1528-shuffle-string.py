1c353186717f9b7356d72b67714dafba31b29d6b
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-07-13 03:43:03 +0200

    Time: 32 ms (98.68%), Space: 13.6 MB (32.89%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -5,7 +5,12 @@ class Solution(object):
         :type indices: List[int]
         :rtype: str
         """
-        res = ''
-        for i in range(0, len(indices)):
-            res += s[indices.index(i)]
-        return res
+        # res = ''
+        # for i in range(0, len(indices)):
+        #     res += s[indices.index(i)]
+        # return res
+        res = [''] * len(s)
+        for i in range(0, len(s)):
+            res[indices[i]] = s[i]
+        return ''.join(res)
+        
