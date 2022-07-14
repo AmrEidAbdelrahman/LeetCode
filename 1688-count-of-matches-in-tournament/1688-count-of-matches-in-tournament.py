aba072a6293963765366d56522f1d1cf91a96acf
Amr Eid <50054520+AmrEidAbdelrahman@users.noreply.github.com>
2022-07-14 14:43:24 +0200

    Time: 34 ms (19.19%), Space: 13.7 MB (14.14%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numberOfMatches(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n >= 2:
+            if n%2 == 0:
+                res += n/2
+                n = n/2
+            else:
+                res += (n-1)/2
+                n = (n-1)/2 + 1
+                print(n)
+        return res
+        
